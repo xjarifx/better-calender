7482c80e58c74b5b046e6c03787f19189ab98417
Jarif <xjarifx@gmail.com>
2026-05-08 21:25:23 +0600

    Fix metro config

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,19 +1,15 @@
-const { getDefaultConfig, mergeConfig } = require('expo/metro-config')
+const { getDefaultConfig } = require('expo/metro-config')
 const path = require('path')
 
 const projectRoot = __dirname
 const workspaceRoot = path.resolve(projectRoot, '../..')
 
-const defaultConfig = getDefaultConfig(projectRoot)
+const config = getDefaultConfig(projectRoot)
 
-const config = {
-  watchFolders: [...defaultConfig.watchFolders, workspaceRoot],
-  resolver: {
-    nodeModulesPaths: [
-      path.resolve(projectRoot, 'node_modules'),
-      path.resolve(workspaceRoot, 'node_modules'),
-    ],
-  },
-}
+config.watchFolders = [...(config.watchFolders || []), workspaceRoot]
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+]
 
-module.exports = mergeConfig(defaultConfig, config)
+module.exports = config
